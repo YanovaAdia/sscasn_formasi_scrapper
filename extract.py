0d044be8ae177605a8b8ff4ca332eb6b81314d31
Yanova <riefkyanova@gmail.com>
2025-02-21 10:52:02 +0700

    Set val and pagination scrap

--- extract.py
+++ extract.py
@@ -1,34 +1,39 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import pandas as pd
 import time
 
-detail = []
+detail = ["S-1/Sarjana", "S-1 PERPUSTAKAAN", "CPNS"]
 
-detail.append(input("Jenjang Pendidikan : "))
-detail.append(input("Program Studi : "))
-detail.append(input("Jenis Pengadaan : "))
+# detail.append(input("Jenjang Pendidikan : "))
+# detail.append(input("Program Studi : "))
+# detail.append(input("Jenis Pengadaan : "))
 
 detail.reverse()
 
 driver = webdriver.Firefox()
 driver.get('https://sscasn.bkn.go.id/')
-driver.implicitly_wait(2)
+time.sleep(3)
+
 
 input_seq = (1,2,4)
 
 for seq in input_seq:
-    in_keys = driver.find_element(By.XPATH, f'//form/div/div[{seq}]/div/div/div/input[1]')
-    in_keys.send_keys(detail.pop())
-    driver.implicitly_wait(1)
+    item = detail.pop()
+    in_keys = driver.find_element(By.XPATH, f'//form/div/div[{seq}]/div/div/div/input')
+    in_keys.click()
+    in_keys.send_keys(item)
+    time.sleep(1)
     set_keys = driver.find_element(By.XPATH, f'//form/div/div[{seq}]/div/div/ul/li[1]')
+    time.sleep(1)
     set_keys.click()
 
-driver.implicitly_wait(2)
+
 set_keys = driver.find_element(By.XPATH, "//form/div/div[5]/a")
 set_keys.click()
-# el_pend = driver.find_element(By.XPATH, "//form/div/div[2]/div/div/div/input[1]")
-# el_pend.send_keys("SMK REKAYASA PERANGKAT LUNAK")
 
-# el_pend = driver.find_element(By.XPATH, "//form/div/div[4]/div/div/div/input[1]")
-# el_pend.send_keys("CPNS")
 
+next_button = driver.find_element(By.CLASS_NAME, "ant-pagination-item-link").is_enabled()
+print(next_button)
